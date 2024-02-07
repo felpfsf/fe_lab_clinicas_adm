3ac037e00aa9e574afe5e3d8fe3425c3f60c982b
Felipe Ferreira <feferreira.sf@gmail.com>
2024-02-07 18:13:11 -0300

    ✨ login page

---
+++ lib/src/pages/login/login_page.dart
@@ -0,0 +1,127 @@
+import 'package:fe_lab_clinicas_adm/src/pages/login/login_controller.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:signals_flutter/signals_flutter.dart';
+import 'package:validatorless/validatorless.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with MessageViewMixin {
+  final controller = Injector.get<LoginController>();
+  final formKey = GlobalKey<FormState>();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+  @override
+  void initState() {
+    messageListener(controller);
+    effect(() {
+      if (controller.logged) {
+        print('Usuário logado');
+        Navigator.of(context).pushReplacementNamed('/home');
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
+
+    return Scaffold(
+      appBar: LabClinicasAppbar(),
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(minHeight: sizeOf.height),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background_login.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(40),
+              constraints: BoxConstraints(maxWidth: sizeOf.width * .4),
+              decoration: BoxDecoration(
+                color: LabClinicasTheme.primaryElement,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    const Text(
+                      'Login',
+                      style: LabClinicasTheme.titleStyle,
+                    ),
+                    const SizedBox(height: 32),
+                    TextFormField(
+                      controller: emailEC,
+                      validator: Validatorless.multiple(
+                        [
+                          Validatorless.required('Email obrigatorio'),
+                          Validatorless.email('Email inválido'),
+                        ],
+                      ),
+                      decoration: const InputDecoration(
+                        label: Text('Email'),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    Watch(
+                      (_) {
+                        return TextFormField(
+                          controller: passwordEC,
+                          obscureText: controller.obscurePassword,
+                          validator:
+                              Validatorless.required('Senha obrigatória'),
+                          decoration: InputDecoration(
+                            label: const Text('Senha'),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                controller.togglePassword();
+                              },
+                              icon: const Icon(Icons.visibility),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 32),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 48,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          final valid =
+                              formKey.currentState?.validate() ?? false;
+                          if (valid) {
+                            controller.login(emailEC.text, passwordEC.text);
+                          }
+                        },
+                        child: const Text('ENTRAR'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/login/login_router.dart
@@ -0,0 +1,25 @@
+import 'package:fe_lab_clinicas_adm/src/pages/login/login_controller.dart';
+import 'package:fe_lab_clinicas_adm/src/pages/login/login_page.dart';
+import 'package:fe_lab_clinicas_adm/src/repositories/user/user_repository.dart';
+import 'package:fe_lab_clinicas_adm/src/repositories/user/user_repository_impl.dart';
+import 'package:fe_lab_clinicas_adm/src/services/login/user_login_service.dart';
+import 'package:fe_lab_clinicas_adm/src/services/login/user_login_service_impl.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class LoginRouter extends FlutterGetItPageRouter {
+  const LoginRouter({super.key});
+
+  @override
+  List<Bind<Object>> get bindings => [
+        Bind.lazySingleton<UserRepository>((i) => UserRepositoryImpl(restClient: i())),
+        Bind.lazySingleton<UserLoginService>((i) => UserLoginServiceImpl(userRepository: i())),
+        Bind.lazySingleton((i) => LoginController(loginService: i())),
+      ];
+
+  @override
+  String get routeName => '/login';
+
+  @override
+  WidgetBuilder get view => (_) => const LoginPage();
+}
